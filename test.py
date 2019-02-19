8130856a38d87605bffbd51b28cc3ddc68020307
engpetermwangi <45688358+engpetermwangi@users.noreply.github.com>
2019-02-19 10:21:47 +0300

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ import unittest
 from calc import add
 
 class TestCalc(unittest.TestCase):
-  def test_add():
-    assertEqual(add(4,1), 5)
+  def test_add(self):
+    self.assertEqual(add(4,1), 5)
 
 if __name__ == "__main__":
   unittest.main()
